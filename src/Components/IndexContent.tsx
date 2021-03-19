791186a8125a4976b88bdc5ea10ddd9d7b1aff19
Anedvago <61481830+Anedvago@users.noreply.github.com>
2021-03-19 13:16:44 -0500

    Implementando react con ts
    añadi material design de boostrap con jquery

--- src/Components/IndexContent.tsx
+++ src/Components/IndexContent.tsx
@@ -1,71 +1,70 @@
 import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-
+import 'bootstrap-material-design'
+import 'jquery'
+import '../indexContent.css';
 import Niños from '../img/FilosofiaNiños 1.png'
 
 const IndexContent: React.FC= () => {
   return (
     <div className="row" id="contenido">
-            <div className="col">
-                <div className="row">
-                    <div className="col-8">
-                        <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <img className="d-block w-100" src={Niños} alt="First slide"></img>
-                                </div>
-                                <div className="carousel-item">
-                                    <img className="d-block w-100" src={Niños} alt="Second slide"></img></div>
-                                </div>
-                                <div className="carousel-item">
-                                    <img className="d-block w-100" src={Niños} alt="Third slide"></img>
+        <div className="col">
+            <div className="row">
+                <div className="col-8">
+                    <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img className="d-block w-100" src={Niños} alt="Second slide"></img>
+                            </div>
+                            <div className="carousel-item">
+                                <img className="d-block w-100" src={Niños} alt="Second slide"></img>
+                            </div>
+                            <div className="carousel-item">
+                                <img className="d-block w-100" src={Niños} alt="Second slide"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-4 cards">
+                    <div className="row">
+                        <div className="col">
+                            <div className="card cards1">
+                                <div className="card-body">
+                                    <h6>
+                                        ¿Forma de aprender?
+                                    </h6>
+                                    <p>
+                                        ¿Acaso existen diferentes formas de aprender? ¡si!, los metodos de aprendizaje son 
+                                        acciones formativas que esperan conseguir la enseñanza de determinados contenidos y 
+                                        la obtención de objetivos específicos. Cada método es diferente en sí, y persigue objetivos 
+                                        distintos. No todos los estudiantes aprenden de la misma manera. A unos el estímulo visual 
+                                        les llega antes, a otros el auditivo, el táctil o el cinestético. Si descubres qué tipo de 
+                                        aprendizaje sigues, podrás conseguir que tu rendimiento académico sea mayor y mejor.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-4 cards">
-                        <div className="row">
-                            <div className="col">
-                                <div className="card cards1">
-                                    <div className="card-body">
-                                      <h6>
-                                          ¿Forma de aprender?
-                                      </h6>
-                                      <p>
-                                          ¿Acaso existen diferentes formas de aprender? ¡si!, los metodos de aprendizaje son 
-                                          acciones formativas que esperan conseguir la enseñanza de determinados contenidos y 
-                                          la obtención de objetivos específicos. Cada método es diferente en sí, y persigue objetivos 
-                                          distintos. No todos los estudiantes aprenden de la misma manera. A unos el estímulo visual 
-                                          les llega antes, a otros el auditivo, el táctil o el cinestético. Si descubres qué tipo de 
-                                          aprendizaje sigues, podrás conseguir que tu rendimiento académico sea mayor y mejor.
-                                      </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col">
-                                <div className="card card2 cards1">
-                                    <div className="card-body">
-                                        <h6>
-                                            ¿Metodos de aprendizaje?
-                                        </h6>
-                                        <p>
-                                            es el conjunto lógico de procedimientos que tienden a dirigir el aprendizaje hacia un objetivo, 
-                                            tema o contenido,  ya sea exponer de manera lógica-secuencial un tema de estudio, resolver problemáticas 
-                                            mediante la investigación, trabajo en equipo,  la colaboración, presentación de resultados. La finalidad de los 
-                                            métodos de aprendizaje es lograr el aprendizaje significativo y el desarrollo de competencias para obtener soluciones 
-                                            pertinentes, creativas e innovadoras.
-                                        </p>
-                                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <div className="card card2 cards1">
+                                <div className="card-body">
+                                    <h6>
+                                        ¿Metodos de aprendizaje?
+                                    </h6>
+                                    <p>
+                                        es el conjunto lógico de procedimientos que tienden a dirigir el aprendizaje hacia un objetivo, 
+                                        tema o contenido,  ya sea exponer de manera lógica-secuencial un tema de estudio, resolver problemáticas 
+                                        mediante la investigación, trabajo en equipo,  la colaboración, presentación de resultados. La finalidad de los 
+                                        métodos de aprendizaje es lograr el aprendizaje significativo y el desarrollo de competencias para obtener soluciones 
+                                        pertinentes, creativas e innovadoras.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="row">
+            </div>
+            <div className="row">
                     <div className="col">
                         <div className="card card2">
                             <div className="card-body">
@@ -78,9 +77,10 @@ const IndexContent: React.FC= () => {
                                  que es estudiar, es enseña a comprender el material para asimilar y repasar un tema en específico.
                               </p>
                             </div>
+                        </div>
                     </div>
-               </div>
-           </div>
+                </div>
+        </div>
    </div>
       
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './Header.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+import 'bootstrap-material-design';
 
 ReactDOM.render(
   <React.StrictMode>
